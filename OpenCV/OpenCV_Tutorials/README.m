cdf592fb1e3eba90ff762e2435c59b4da68299ff
Avneesh Mishra <123avneesh@gmail.com>
2018-06-25 17:37:34 +0530

    Update README.m

--- OpenCV/OpenCV_Tutorials/README.m
+++ OpenCV/OpenCV_Tutorials/README.m
@@ -1,2 +1,1 @@
-Binder file [here](https://mybinder.org/v2/gh/TheProjectsGuy/Blogger/master?urlpath=https%3A%2F%2Fgithub.com%2FTheProjectsGuy%2FBlogger%2Fblob%2Fmaster%2FOpenCV%2FOpenCV_Tutorials%2FSample-OpenCV.ipynb)
-[Binder](https://mybinder.org/badge.svg)
+*This is code directory for OpenCV tutorials*
